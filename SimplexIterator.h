8fca83f4586a96477247b8d06b58f08c7c1531b4
Ionut Georgescu <igeorges@uci.edu>
2013-02-26 16:07:40 -0800

    Iterator for an N-dimensional simplex
    for (i=0; i<M; i++) {
    	for (j=i+1; j<M; j++) {
    		for (k=j+1; k<M; k++) {
    			...

---
+++ SimplexIterator.h
@@ -0,0 +1,14 @@
+//
+//  SimplexIterator.h
+//  SCP_Double_Excitations
+//
+//  Created by Ionut Georgescu on 2/25/13.
+//
+//
+
+#ifndef SCP_Double_Excitations_SimplexIterator_h
+#define SCP_Double_Excitations_SimplexIterator_h
+
+
+
+#endif
