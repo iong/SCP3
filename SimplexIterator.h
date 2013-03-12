c636ff2fae27fc73b7775b8c4bb19e026b89d794
Ionut Georgescu <igeorges@uci.edu>
2013-03-11 17:31:25 -0700

    Compact class declaration.

--- SimplexIterator.h
+++ SimplexIterator.h
@@ -30,82 +30,103 @@ class SimplexIterator
 public:
     int index[dim];
 
-    SimplexIterator(int N_) : N(N_)
-    {
-        int i, j;
-
-        last_location = 0;
-
-        for (i=0; i<dim; i++) {
-            V[i].resize(N);
-            V[i][i] = 1;
-        }
-
-        for (i=0; i<N; i++) {
-            V[0][i] = i+1;
-        }
-
-        for (i=1; i<dim; i++) {
-            for (j=i+1; j<N; j++) {
-                V[i][j] = V[i][j-1] + V[i-1][j-1];
-            }
-        }
-
-        for (i=0; i<dim; i++) index[i] = i;
-    }
-
-    SimplexIterator& operator=(int location)
-    {
-        int d, j;
-        if (location == last_location) {
-            return *this;
-        }
-        else if (location == last_location + 1) {
-            (*this)++;
-
-            return *this;
-        }
-
-        last_location = location;
-
-        fill(index, index+dim, 0);
-
-        for (j=0, d=dim-1; d>0; d--) {
-            for (; j <= N - d && location >= V[d-1][(N - 1) - (j + 1)]; j++) {
-                    location -= V[d-1][ (N - 1) - (j + 1) ];
-            }
-            index[dim - 1 - d] = j++;
-        }
-        index[dim - 1] = j + location;
-
-        return *this;
-    }
-
-    SimplexIterator operator++(int)
-    {
-        int d;
-
-        index[dim-1]++;
-        for (d=dim-1; d>0 && index[d]>=N + d - (dim-1); d--) {
-                index[d] = 0;
-                index[d-1]++;
-        }
-        if (index[0] > N-dim) {
-            cerr << "Simplex incremented out of bounds.\n";
-            exit(EXIT_FAILURE);
-        }
-        for (d=d+1; d<dim; d++) {
-            index[d] = index[d-1]+1;
-        }
-
-        last_location++;
-
-        return *this;
-    }
+    SimplexIterator(int N_);
+    SimplexIterator<dim>& operator=(int location);
+    SimplexIterator operator++(int);
 
     const int& end() {
         return V[dim-1][N-1];
     }
 };
+
+template<int dim>
+ostream& operator<<(ostream& os, SimplexIterator<dim>& it)
+{
+    os << "(";
+    for (int i=0; i<dim-1; i++) {
+        os<<it.index[i] << ", ";
+    }
+    os << it.index[dim-1] << ")";
+    
+    return os;
+}
+
+template<int dim>
+SimplexIterator<dim>::SimplexIterator(int N_) : N(N_)
+{
+    int i, j;
+    
+    last_location = 0;
+    
+    for (i=0; i<dim; i++) {
+        V[i].resize(N);
+        V[i][i] = 1;
+    }
+    
+    for (i=0; i<N; i++) {
+        V[0][i] = i+1;
+    }
+    
+    for (i=1; i<dim; i++) {
+        for (j=i+1; j<N; j++) {
+            V[i][j] = V[i][j-1] + V[i-1][j-1];
+        }
+    }
+    
+    for (i=0; i<dim; i++) index[i] = i;
+}
+
+
+template<int dim>
+SimplexIterator<dim>& SimplexIterator<dim>::operator=(int location)
+{
+    int d, j;
+    if (location == last_location) {
+        return *this;
+    }
+    else if (location == last_location + 1) {
+        (*this)++;
+        
+        return *this;
+    }
+    
+    last_location = location;
+    
+    fill(index, index+dim, 0);
+    
+    for (j=0, d=dim-1; d>0; d--) {
+        for (; j <= N - d && location >= V[d-1][(N - 1) - (j + 1)]; j++) {
+            location -= V[d-1][ (N - 1) - (j + 1) ];
+        }
+        index[dim - 1 - d] = j++;
+    }
+    index[dim - 1] = j + location;
+    
+    return *this;
+}
+
+
+template<int dim>
+SimplexIterator<dim> SimplexIterator<dim>::operator++(int)
+{
+    int d;
+    
+    index[dim-1]++;
+    for (d=dim-1; d>0 && index[d]>=N + d - (dim-1); d--) {
+        index[d] = 0;
+        index[d-1]++;
+    }
+    if (index[0] > N-dim) {
+        cerr << "Simplex incremented out of bounds.\n";
+        exit(EXIT_FAILURE);
+    }
+    for (d=d+1; d<dim; d++) {
+        index[d] = index[d-1]+1;
+    }
+    
+    last_location++;
+    
+    return *this;
+}
 
 #endif
