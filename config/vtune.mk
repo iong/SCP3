0f7c6af198fbd0ef0168333a82459d9c1eb9008c
Ionut Georgescu <ionut.georgescu@uci.edu>
2013-04-17 17:43:38 -0700

    Compilation profile for WHBB. White space in vtune.mk

--- config/vtune.mk
+++ config/vtune.mk
@@ -1,6 +1,4 @@
+COMPILER := intel
 BLAS := mkl_sequential
 COPT := -O2 -g
 FOPT := -O2 -g
-COMPILER := intel
-
-
---
+++ config/whbb.mk
@@ -0,0 +1,6 @@
+COMPILER:=intel
+BLAS:=mkl_parallel
+COPT:=-O3 -no-prec-div -xHost
+FOPT:=$(COPT)
+WHBB:=1
+
