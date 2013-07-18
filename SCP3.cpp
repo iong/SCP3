add6b9b5d5669608ed4aa997aba2c77c8558f4e3
Ionut Georgescu <ionut.georgescu@uci.edu>
2013-07-18 11:16:44 -0700

    Initilialize X3B

--- SCP3.cpp
+++ SCP3.cpp
@@ -387,6 +387,7 @@ int main (int argc, char *  argv[]) {
         ps::pot_nasa_init();
         h2o::fortran::pes2b_init();
         h2o::fortran::pes3b_init();
+        h2o::x3b_bits::load("x3b.nc");
     }
 #endif
 
