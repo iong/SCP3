7b9608c6e8a414332df29c2f7593bd18bc36e730
Ionut Georgescu <ionut.georgescu@uci.edu>
2013-07-22 19:13:05 -0700

    Use MPI to evaluate the potential in parallel.

--- SCP3.cpp
+++ SCP3.cpp
@@ -16,11 +16,7 @@
 
 #include <getopt.h>
 
-/*
-#ifdef OPENMP
-#include <omp.h>
-#endif
-*/
+#include <mpi.h>
 
 #include <armadillo>
 
@@ -44,7 +40,6 @@ static struct option program_options[] = {
     { "triples", required_argument, NULL, '3'},
     { "PES", required_argument, NULL, 'p'},
     { "select-modes", required_argument, NULL, 'm'},
-    { "distributed", required_argument, NULL, 'd'},
     { "no-gradient", no_argument, NULL, 'G'},
     { "block-width", required_argument, NULL, 'B'},
     {NULL, 0, NULL, 0}
@@ -62,8 +57,8 @@ static uvec selected_modes;
 
 static bool use_gradient = true;
 
-static int segment_id = 0;
-static int nb_segments = 1;
+static int myrank = 0;
+static int nprocs = 1;
 
 static int block_width = 1024;
 
@@ -102,7 +97,7 @@ void parse_mode_description(const string& desc, uvec& selected_modes)
 void process_options(int argc,  char *  argv[])
 {
     int ch;
-    while ( (ch = getopt_long(argc, argv, "S:N:2:3:r:p:m:d:GB:", program_options, NULL)) != -1) {
+    while ( (ch = getopt_long(argc, argv, "S:N:2:3:r:p:m:GB:", program_options, NULL)) != -1) {
         int p2;
         switch (ch) {
             case 'N':
@@ -135,10 +130,6 @@ void process_options(int argc,  char *  argv[])
                 break;
             case 'm':
                 parse_mode_description(optarg, selected_modes);
-                break;
-            case 'd':
-                segment_id = atoi(optarg);
-                nb_segments = atoi(strrchr(optarg, '/') + 1);
                 break;
             case 'G':
                 use_gradient = false;
@@ -178,105 +169,113 @@ void SCP3_a(const string& h2o_pes, const vec& x0, const vec& omega, const mat& M
     ScaledMatrixElements  sme(omega(modes), Nmodes2, Nmodes3);
     int Nstates = sme.getBasisSize();
     
-    mat M(Nstates, Nstates);
-    M.fill(0.0);
-    
+    mat M;
     ofstream E0out_sd, E0out_t;
-    if (nb_segments > 1) {
-        char s[32];
-        sprintf(s, "E0_sd-%03d.dat", segment_id);
-        E0out_sd.open(s);
-    }
-    else {
+    if (myrank ==0) {
+        M.zeros(Nstates, Nstates);
         E0out_sd.open("E0_sd.dat");
         E0out_t .open("E0_t.dat" );
+
+        fixed(E0out_sd);
+        E0out_sd.precision(10);
+        
+        fixed(E0out_t);
+        E0out_t.precision(10);
     }
-
-
-    fixed(E0out_sd);
-    E0out_sd.precision(10);
     
-    fixed(E0out_t);
-    E0out_t.precision(10);
-    
-    double E0[3];
  
-    int seq_len = NSobol / nb_segments;
-    int seq_start = seq_len * segment_id;
-    int seq_stop = seq_start + seq_len;
-
     int nw = x0.n_rows / 9;
     
-    vec bV(block_width);
-    mat by(Nmodes0, block_width), bVy(Nmodes0, block_width); 
+    int local_work = block_width / nprocs;
+    vec bV(local_work), global_bV;
+    mat by(Nmodes0, local_work), global_by,
+        bVy(Nmodes0, local_work), global_bVy; 
 
-    if (seq_start % block_width != 0) {
-        cerr << "seq_start must me a multiple of block_width!\n";
-        exit(EXIT_FAILURE);
-    }
+    for (int i = 0; i < NSobol; i += block_width) {
+        MPI::COMM_WORLD.Barrier();
 
-    for (int i = 0; i < seq_stop; i += block_width) {
+        int local_start = i + local_work * myrank;
+        int local_stop = local_start + local_work;
+
         if (rng_in.is_open()) {
-            for (int j=0; j < block_width; j++) {
+            for (int j=i; j < local_start; j++) {
+                double tmp;
+                for (int k=0; k<Nmodes0; k++) rng_in >> tmp; 
+            }
+            for (int j=0; j < local_work; j++) {
                 for (int k=0; k<Nmodes0; k++) {
                     rng_in >> by(k, j);
                 }
             }
         }
         else {
-            for (int j=0; j < block_width; j++) {
-                sobol::std_normal(by.n_rows, &sobol_skip, by.colptr(j));
+            for (int j=0; j < local_work; j++) {
+                long long int current_skip = sobol_skip + local_start + j;
+                sobol::std_normal(by.n_rows, &current_skip, by.colptr(j));
             }
         }
-
-        if (i < seq_start ) continue;
 
         by /=  sqrt(2.0);
 
-//#pragma omp parallel
-        {
-//#ifdef OPENMP
-//            omp_set_nested(false);
-//#endif
+        h2o::PES *pot = h2o::PESFromString(h2o_pes);
 
-            h2o::PES *pot = h2o::PESFromString(h2o_pes);
+        for (int j = 0; j < local_work; j++) {
+            double V;
 
-//#pragma omp for schedule(static)
-            for (int j = 0; j < block_width; j++) {
-                double V;
+            vec r = bohr *(x0 + MUa*by.col(j));
+            
+            if (use_gradient) {
+                vec Vr(r.n_rows);
 
-                vec r = bohr *(x0 + MUa*by.col(j));
+                V = (*pot)(nw, r.memptr(), Vr.memptr()) / autokcalpmol;
+                Vr *= bohr/autokcalpmol;
                 
-                if (use_gradient) {
-                    vec Vr(r.n_rows);
-
-                    V = (*pot)(nw, r.memptr(), Vr.memptr()) / autokcalpmol;
-                    Vr *= bohr/autokcalpmol;
-                    
-                    bVy.col(j) = MUa.t() * Vr - omega % by.col(j);
-                }
-                else {
-                    V = (*pot)(nw, r.memptr()) / autokcalpmol;
-                }
-
-                bV[j] = V - 0.5 * dot(by.col(j), omega % by.col(j));
-
+                bVy.col(j) = MUa.t() * Vr - omega % by.col(j);
+            }
+            else {
+                V = (*pot)(nw, r.memptr()) / autokcalpmol;
             }
 
-            delete pot;
+            bV[j] = V - 0.5 * dot(by.col(j), omega % by.col(j));
+
         }
+
+        delete pot;
+
+        if (myrank == 0) {
+            global_bV.zeros(block_width);
+            global_by.zeros(Nmodes0, block_width);
+            global_bVy.zeros(Nmodes0, block_width);
+        }
+
+        MPI::COMM_WORLD.Gather(bV.memptr(), local_work, MPI::DOUBLE,
+                global_bV.memptr(), local_work, MPI::DOUBLE, 0);
+
+        MPI::COMM_WORLD.Gather(
+                by.memptr(), by.n_rows * local_work, MPI::DOUBLE,
+                global_by.memptr(), by.n_rows * local_work, MPI::DOUBLE, 0);
+
+        MPI::COMM_WORLD.Gather(
+                bVy.memptr(), bVy.n_rows * local_work, MPI::DOUBLE,
+                global_bVy.memptr(), bVy.n_rows * local_work, MPI::DOUBLE, 0);
+
+
+        if (myrank != 0) continue;
 
         cout << "finished pot eval\n";
             
         if (use_gradient) {
-            sme.addEpot(by.rows(modes), bV, bVy.rows(modes), M);
+            sme.addEpot(global_by.rows(modes), global_bV,
+                    global_bVy.rows(modes), M);
         }
         else {
-            sme.addEpot(by.rows(modes), bV, M);
+            sme.addEpot(global_by.rows(modes), global_bV, M);
         }
 
         if ( (i+block_width)%(1<<14)==0) {
-            mat Mout = M / (i+block_width - seq_start);
+            double E0[3];
+
+            mat Mout = M / (i + block_width);
             sme.addHODiagonal(Mout);
             Mout.diag() += 0.5 * (sum(omega) - sum(omega(modes)));
 
@@ -291,7 +290,7 @@ void SCP3_a(const string& h2o_pes, const vec& x0, const vec& omega, const mat& M
             E0out_sd << i+block_width << " " << E0[0] <<" "<< E0[1] <<" "
             << E0[1] - E0[0] << endl;
             
-            if ( (i+block_width)%(1<<17)==0 && nb_segments == 1) {
+            if ( (i+block_width)%(1<<17)==0) {
                 char s[128];
                 sprintf(s, "sM_%07d.h5", i+block_width);
                 save_hdf5(Mout, s);
@@ -304,21 +303,29 @@ void SCP3_a(const string& h2o_pes, const vec& x0, const vec& omega, const mat& M
             }
         }
     }
+
+    if (myrank != 0) return;
+
     E0out_sd.close();
     E0out_t.close();
     
-    M /= seq_len;
+    M /= NSobol;
     sme.addHODiagonal(M);
     M.diag() += 0.5 * (sum(omega) - sum(omega(modes)));
-    
-    if (nb_segments > 1) {
-        char s[128];
-        sprintf(s, "sM_%07d.h5", seq_stop);
-        save_hdf5(M, s);
-    }
 }
 
-int main (int argc, char *  argv[]) {
+int main (int argc, char *  argv[])
+{
+    int provided = MPI::Init_thread(argc, argv, MPI::THREAD_FUNNELED);
+
+    if (provided < MPI::THREAD_FUNNELED) {
+        cerr << "Fucked!\n" << provided <<" "<< MPI::THREAD_FUNNELED <<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    myrank   = MPI::COMM_WORLD.Get_rank();
+    nprocs = MPI::COMM_WORLD.Get_size();
+
     process_options(argc, argv);
 
 #ifdef HAVE_BOWMAN
@@ -347,7 +354,7 @@ int main (int argc, char *  argv[]) {
     mat U;
     eig_sym(omegasq0, U, H);
 
-    if (segment_id == 0) {
+    if (myrank == 0) {
         ofstream sout("omega0.dat");
         sout << sqrt(abs(omegasq0))*autocm << endl;
         sout.close();
@@ -372,4 +379,6 @@ int main (int argc, char *  argv[]) {
     SCP3_a(h2o_pes, x0, omega, MUa, selected_modes);
     
     rng_in.close();
+
+    MPI::Finalize();
 }
